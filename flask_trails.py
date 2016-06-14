cac3294055f98a7764b5bbe5936d9281434297cf
Abhishek Upadhyay <abhishek.upadhyay.cse12@iitbhu.ac.in>
2016-06-14 15:04:19 +0530

    bugfixes

--- flask_trails.py
+++ flask_trails.py
@@ -1,4 +1,6 @@
 import json
+import logging
+import traceback
 
 from bson import json_util
 from bson.objectid import ObjectId
@@ -11,31 +13,48 @@ app = Flask(__name__)
 app.config['MONGO_DBNAME'] = "hiking_trails"
 mongo = PyMongo(app, config_prefix='MONGO')
 
+
 def toJson(data):
     return json.dumps(data, default=json_util.default)
 
 
 @app.route('/trails/create', methods=['POST'])
 def create_trail():
-    print request.get_data()
-    print type(request.get_json())
     trails = mongo.db.trails
-    trail_id = trails.insert_one(request.get_json()).inserted_id
-    response  = {}
+    try:
+        trail_id = trails.insert_one(request.get_json()).inserted_id
+    except Exception as e:
+        response = {}
+        response["result"] = "error"
+        data = toJson(response)
+        traceback.print_exc(e)
+        logging.warning(e)
+        return Response(data, status=408, mimetype='application/json')
+
+    logging.info(trail_id)
+    response = {}
     response["result"] = "success"
     response["trailId"] = str(trail_id)
     data = toJson(response)
     return Response(data, status=200, mimetype='application/json')
 
+
 @app.route('/trails/get/<trailid>', methods=['GET'])
 def get_trail(trailid):
     trails = mongo.db.trails
-    trail = trails.find_one({"_id": ObjectId(trailid)})
-    print trail
-    print type(trail)
-    import pdb; pdb.set_trace()
-    data = toJson(trail)
-    return Response(data, status=200, content_type='application/json')
+    status_code = 200
+    try:
+        response = trails.find_one({"_id": ObjectId(trailid)})
+    except Exception as e:
+        logging.warning(e)
+        traceback.print_exc(e)
+        response = {}
+        response["result"] = "error"
+        status_code = 408
+
+    data = toJson(response)
+    return Response(data, status=status_code, content_type='application/json')
+
 
 @app.route('/trails/search', methods=['GET'])
 def search_trail():
@@ -43,23 +62,29 @@ def search_trail():
     lon = request.args.get('lon')
     lat = float(lat)
     lon = float(lon)
-    print type(lat)
-    print "from print %s" %lat
+    logging.info(lat)
     query = {"startingPoint":
-                { "$near" :
+                {"$near":
                     {
-                        "$geometry": { "type": "Point", "coordinates": [ lat, lon ] },
+                        "$geometry": {"type": "Point", "coordinates": [lat, lon]},
                         "$minDistance": 0,
                         "$maxDistance": 10000
                     }
                 }
             }
-    result = mongo.db.trails.find(query);
-    #import pdb; pdb.set_trace()
-    print result.count()
+    status_code = 200
+    try:
+        result = mongo.db.trails.find(query)
+    except Exception as e:
+        logging.warning(e)
+        traceback.print_exc(e)
+        result = {}
+        result["result"] = "error"
+        status_code = 408
+
     data = dumps(result)
 
-    return Response(data, status=200, content_type='application/json')
-        
+    return Response(data, status=status_code, content_type='application/json')
+
 if __name__ == '__main__':
     app.run()
